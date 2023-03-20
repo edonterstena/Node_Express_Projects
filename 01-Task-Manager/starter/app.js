eaf0f7e0108e946619debc35e3d2ca1e9c3d5796
Edon Tërstena <63924185+edonterstena@users.noreply.github.com>
2023-03-20 21:13:00 +0100

    last commit

--- 01-Task-Manager/starter/app.js
+++ 01-Task-Manager/starter/app.js
@@ -1,1 +1,30 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("../starter/routes/tasks");
+const connectDb = require("./db/connect");
+require("dotenv").config();
+const notFound = require("../starter/middleware/not-found");
+const errorHandlerMiddleware = require("../starter/middleware/error-handler");
+
+//middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+// routes
+app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDb(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
---
+++ 01-Task-Manager/starter/routes/tasks.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controller/tasks");
+
+const router = express.Router();
+
+router.get("/", getAllTasks);
+router.post("/", createTask);
+router.get("/:id", getTask);
+router.patch("/:id", updateTask);
+router.delete("/:id", deleteTask);
+
+module.exports = router;
