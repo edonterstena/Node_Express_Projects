ee61f5e091b4fcdb0a1562c79c32c361260e069c
Edon Tërstena <63924185+edonterstena@users.noreply.github.com>
2023-03-26 01:43:09 +0100

    socket basic example

---
+++ Socket.io/client/socket/src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  const sendMessage = () => {
+    socket.emit("send_message", { message: message });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+  return (
+    <div className="App">
+      <input
+        placeholder="enter message.."
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      />
+      <button onClick={sendMessage}>Send message</button>
+      <h1>Message:</h1>
+      {messageReceived}
+    </div>
+  );
+}
+
+export default App;
